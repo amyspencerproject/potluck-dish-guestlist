81e5f6b6aade02c4b3c27358336d2f69d44d9e4a
amyspencerproject <amy@spencerproject.com>
2021-09-14 18:35:26 -0600

    Added event listener and functions to addGuestButton

--- js/script.js
+++ js/script.js
@@ -11,3 +11,34 @@ const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
 
+addGuestButton.addEventListener("click", function(){
+    const guest = guestInput.value; //grab the input field when user clicks on button
+    
+    if (guest !== "") {
+      addToList(guest);
+      updateGuestCount();
+    }
+    clearInput(); //clear guest name
+});
+
+const clearInput = function(){
+    guestInput.value = ""; //need to use .value to clear html input field
+};
+
+const addToList = function(guest) {
+    const listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
+};
+
+const updateGuestCount = function() {
+    const guests = document.querySelectorAll(".guest-list li") //selects all the li in the guest-list class
+    guestCount.innerText = guests.length;
+    if (guests.length === 8) {
+        addGuestButton.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestFull.classList.remove("hide");
+    }
+};
+
